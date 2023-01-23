c91d156b64cea92442fcd18de8f7093da491d6bd
Lindjunne Montenegro II <lgbmontenegroii@outlook.ph>
2023-01-24 00:04:05 +0800

    Daisy UI Template
    Signed-off-by: Lindjunne Montenegro II <lgbmontenegroii@outlook.ph>

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css'
+</script>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
+
+<h1 class="text-3xl font-bold underline">
+    Welcome to SvelteKit
+</h1>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="btn btn-accent">Hello daisyUI</button>
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/routes/**/*.{svelte,ts}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require( 'daisyui' ),
+    ],
+}
